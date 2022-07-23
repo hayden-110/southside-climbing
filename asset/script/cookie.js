98fdb3993efdb0bdbd4222f2455da4c53d219fb1
hayden-110 <99691272+hayden-110@users.noreply.github.com>
2022-07-23 12:51:41 +0100

    removed mjs for js

--- asset/script/cookie.js
+++ asset/script/cookie.js
@@ -1,4 +1,4 @@
-import Cookies from './node_modules/js-cookie/dist/js.cookie.mjs'
+import Cookies from './node_modules/js-cookie/dist/js.cookie.js'
 
 const banner = document.querySelector('.cookie-pop');
 const acceptAll = document.querySelectorAll('#acceptAll');
